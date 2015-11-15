86721e46fa5c3a2770569bc43a8b9b84b955f52f
chenshukun <244410894@qq.com>
2015-11-15 23:26:00 +0800

    add autolayout demo

---
+++ autolayout/自动布局/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  自动布局
+//
+//  Created by ChenShuKun on 15/11/15.
+//  Copyright © 2015年 ChenShuKun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
