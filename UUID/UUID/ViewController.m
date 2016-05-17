3e64a0460257d1e17b221a6aff2f43ab390e4d59
chenshukun <244410894@qq.com>
2016-05-17 21:12:52 +0800

    c

--- UUID/UUID/ViewController.m
+++ UUID/UUID/ViewController.m
@@ -30,10 +30,41 @@
     
     NSLog(@"get UUID == %@",[ViewController UUID]);
     //16DCB176-BFDB-46CC-946C-9DF40C0B5367
-
+    [self  hongTest];
+}
+- (void)hongTest {
     
+    NSLog(@"== %d",__IPHONE_OS_VERSION_MAX_ALLOWED);//获取最大的 系统版本号 
+    NSLog(@"== %d",__IPHONE_OS_VERSION_MIN_REQUIRED);//设置中的deployment target，是可变的，根据开发的设置有所不同
+    
+    if (__IPHONE_9_1 > __IPHONE_OS_VERSION_MIN_REQUIRED) {
+        NSLog(@"版本 xiaoyu 9.0");
+    }else {
+        NSLog(@"版本大约 9.0");
+    }
 }
 
+- (void)test {
+    /**
+     *  @author ChenShuKun, 16-05-12 23:05:02
+     *
+     *  从这个函数名称，大概可以猜出来这个函数的意思是字符串转换成无符号长整型。如何看出来，这么看：str-to-ul，再补全就是string-to-unsign long。
+     
+     这是C语言函数，参数说明如下：
+     
+     参数一：const char *类型，表示字符串的起始地址
+     参数二：表示字符串有效数字的结束地址，传0或者NULL表示不接收
+     参数三：转换基数。当base=0,自动判断字符串的类型，并按10进制输出，例如0xa,就会把字符串当做16进制处理，输出的为10
+     *
+     *  @param 0
+     *  @param 1
+     *
+     *  @return
+     */
+    unsigned long log =  strtoul([[@"1" substringWithRange:NSMakeRange(0, 1)] UTF8String], 0, 10);
+    printf("==== %lu",log);
+
+}
 
 + (NSString *)UUID {
     //com.huangyibiao.test.group
--- sorted/冒泡排序/冒泡排序/ViewController.m
+++ sorted/冒泡排序/冒泡排序/ViewController.m
@@ -16,7 +16,130 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+ 
+    /*
+     1 思路 ( 小 --> 大)
+        两层遍历循环  两两 比较  如果前一个 比后一个大, 调换
+        时间复杂度 o (n*n)
+     */
+    
+    [self test1];
+    
+    [self soreder];
+    
+//    [self test];
+}
+
+- (void)test1 {
+    CFAbsoluteTime beforeTime = CFAbsoluteTimeGetCurrent();
+    
+
+    NSMutableArray *array = [NSMutableArray arrayWithArray:[self array]];
+    for (int i = 0 ; i < array.count; i++) {
+        
+        for (int j = 0 ; j < array.count-i-1; j++) {
+            
+            if ([array[j+1] integerValue] < [array[j] integerValue]) {
+                
+                NSInteger temp = [array[j] integerValue];
+                array[j] = array[j+1];
+                array[j+1]= [NSNumber numberWithInteger:temp];
+                
+            }
+        }
+        
+    }
+    
+    
+    CFAbsoluteTime afterTime = CFAbsoluteTimeGetCurrent();
+    NSLog(@" after = %@ ",array);
+    NSLog(@" time---%f ",afterTime - beforeTime);// time---0.000712
+
+}
+
+- (void)test {
+    
+     int a[5] = {5,-3,8,6,4};
+     sort(a,sizeof(a) / sizeof(int));
+     
+     for (int i = 0; i < sizeof(a) / sizeof(int); ++i) {
+     NSLog(@"%d", a[i]);
+     }
+    
+}
+//C 语言版 冒泡
+void sort(int a[],int len ){
+    for (int i = 0; i < len ; i++) {
+        
+        for (int j =  0 ; j < len-1-i; j++) {
+            if (a[j] > a[j+1]) {
+                int temp = a[j];
+                a[j]= a[j+1];
+                a[j+1] = temp;
+            }
+        }
+        
+    }
+}
+
+// 冒泡排序算法的改进
+//用一个 标识符来 表示 ,是否需要 继续 下移下去
+- (void)soreder {
+    
+    CFAbsoluteTime beforeTime = CFAbsoluteTimeGetCurrent();
+    
+    NSMutableArray *array = [NSMutableArray arrayWithArray:[self array]];
+    NSInteger last = [array count]-1;
+    
+    while (last > 0) {
+        NSInteger top = 0;
+        for (int index = 0 ; index < last; index++) {
+            if ([array[index] integerValue] > [array[index+1] integerValue]) {
+                top = index;
+                
+                NSInteger temp = [array[index] integerValue];
+                array[index] = array[index+1];
+                array[index+1]= [NSNumber numberWithInteger:temp];
+
+            }
+           
+        }
+        
+        last = top;
+    }
+    
+    CFAbsoluteTime afterTime = CFAbsoluteTimeGetCurrent();
+    NSLog(@" time---%f ",afterTime - beforeTime);//time---0.000017
+   
+    NSLog(@"sort arry = %@",array);
+    
+}
+
+//再次改进
+/* 
+ 传统冒泡排序中每一趟排序操作只能找到一个最大值或最小值,我们考虑利用在每趟排序中进行正向和反向两遍冒泡的方法一次可以得到两个最终值(最大者和最小者) , 从而使排序趟数几乎减少了一半。
+*/
+
+/*void Bubble_2 ( int r[], int n){
+ int low = 0;
+ int high= n -1; //设置变量的初始值
+ int tmp,j;
+ while (low < high) {
+ for (j= low; j< high; ++j) //正向冒泡,找到最大者
+ if (r[j]> r[j+1]) {
+ tmp = r[j]; r[j]=r[j+1];r[j+1]=tmp;
+ }
+ --high;                 //修改high值, 前移一位
+ for ( j=high; j>low; --j) //反向冒泡,找到最小者
+ if (r[j]<r[j-1]) {
+ tmp = r[j]; r[j]=r[j-1];r[j-1]=tmp;
+ }
+ ++low;                  //修改low值,后移一位
+ }
+ }   */
+- (NSArray *)array {
+    
+    return @[@1,@100,@(-11),@0,@1,@999,@81,@2,@101,@3,@5,@4,@20];
 }
 
 - (void)didReceiveMemoryWarning {
